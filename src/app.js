eb36c98f841ee4aa725fe4ea6c97b3b2a44e7421
Gaush1 <gaush5u786@gmail.com>
2023-11-27 19:13:37 +0530

    first commit

---
+++ src/app.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+
+
+const app = express();
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}));
+
+app.use(express.json({limit:"16kb"})); // Build-in middleware to convert req.body into json
+
+app.use(express.urlencoded({extended: true, limit: "16kb"})); // Build-in middleware to read data from url.
+
+app.use(express.static("public")); // Build-in middleware to read static files
+
+app.use(cookieParser()); // Third party middleware to perform operations on cookies
+
+export { app }  
---
+++ src/index.js
@@ -0,0 +1,42 @@
+// require('dotenv').config({path: './env'})
+
+import dotenv from 'dotenv';
+// import mongoose from 'mongoose';
+// import { DB_NAME } from './constants';
+import connectDB from './db/index.js';
+import app from './app.js'
+
+dotenv.config({
+    path: './env'
+})
+
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`Listening on ${process.env.PORT}`)
+    })
+})
+.catch((error)=>console.log("MongoDb connection failed !!! ", error))
+
+/*
+import express from 'express';
+const app = express();
+
+(async ()=>{
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("error",(error)=>{
+            console.log("Errore: ",error);
+            throw error;
+        })
+
+        app.listen(process.env.PORT, ()=>{
+            console.log(`Listening on ${process.env.PORT}`);
+        })
+        
+    } catch (error) {
+        console.error("Error: ",error)
+        throw error;
+    }
+})();
+*/
