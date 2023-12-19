f3843d17c0c5e48bb128de5e9a75c1f6ba63649a
Gaush1 <gaush5u786@gmail.com>
2023-12-19 17:58:28 +0530

    Multer and Cloudnary added.

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,5 +1,6 @@
-import multer from "multer";
+import multer from "multer"; 
 
+// Multer used to store file.
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
       cb(null, './public/temp')
